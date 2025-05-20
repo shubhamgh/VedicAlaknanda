4b562f3e7086ca030e434a9a1894a35f44913fb3
shubhamgh <shubhamm191200@gmail.com>
2025-05-20 20:10:21 +0530

    Contact component change

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,6 +4,7 @@ import React from "react";
 const Contact = () => {
   return (
     <div className="p-6 max-w-3xl mx-auto">
+      <h1>UNDER DEVELOPMENT</h1>
       <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
       <p className="text-lg mb-2">
         <strong>Address:</strong> Narkota, near Rudraprayag, Uttarakhand
